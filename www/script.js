a3978ec096a9cbb8088e6ef5e6f34ea56638b3b6
Martin Weidauer <weidauer@de.ibm.com>
2018-03-21 17:26:37 +0000

    call of feedback_like API try #1 - like / disklike button (instead of image) - feedback_like function

--- www/script.js
+++ www/script.js
@@ -222,4 +222,26 @@
         value
     }) => `<div onclick="chatbuttonClicked('${value || label}')" class="chatbutton" value="${value || label}">${label || value}</div>`;
 
+
+	function feedback_like() {
+        $.ajax( {
+            url: '/feedback_like',
+            type: 'POST',
+            data: {'message': text , 'context': context},
+            ContentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success:function(data){
+                context = data.context;
+                addMessage(data.output.text, 'left', data.intents[0].intent);
+            },
+            error:function(XMLHttpRequest,status,error){
+                //do nothing
+            }
+        })
+	}
+
+	function feedback_dislike() {
+		
+	}
+
 }.call(this));
